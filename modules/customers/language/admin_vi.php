8b0775d7c17caee10c6a3c7f800680994376842c
unknown <candy@.(none)>
2019-03-16 19:55:20 +0700

    update code

--- modules/customers/language/admin_vi.php
+++ modules/customers/language/admin_vi.php
@@ -1,13 +1,12 @@
 <?php
 
-
-if (!defined('NV_MAINFILE'))
+if (!defined('NV_ADMIN') or !defined('NV_MAINFILE'))
     die('Stop!!!');
  
 $lang_translator['author'] = 'NV Systems (hoangnt@nguyenvan.vn)';
-$lang_translator['createdate'] = '14/3/2019, 17:22';
+$lang_translator['createdate'] = '08/10/2014, 15:22';
 $lang_translator['copyright'] = '@Copyright (C) 2014 NV Systems LLC. All rights reserved';
 $lang_translator['info'] = '';
 $lang_translator['langtype'] = 'lang_module';
  
-$lang_module['nocatpage'] = 'Error:No topic';  // {LANG.nocatpage}  , $xtpl->assign('LANG', $lang_module);
+$lang_module['main'] = 'Quản lý Khác Hàng';
--- modules/customers/language/vi.php
+++ modules/customers/language/vi.php
@@ -5,7 +5,7 @@ if (!defined('NV_MAINFILE'))
     die('Stop!!!');
  
 $lang_translator['author'] = 'NV Systems (hoangnt@nguyenvan.vn)';
-$lang_translator['createdate'] = '14/3/2019, 17:22';
+$lang_translator['createdate'] = '08/10/2014, 15:22';
 $lang_translator['copyright'] = '@Copyright (C) 2014 NV Systems LLC. All rights reserved';
 $lang_translator['info'] = '';
 $lang_translator['langtype'] = 'lang_module';
