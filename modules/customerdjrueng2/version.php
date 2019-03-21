92d54b6df80f86e3162e9739c5035b4ae562d61a
Djrueng Nay <djruengnay@gmail.com>
2019-03-21 22:40:18 +0700

    tao modules 2

--- modules/customerdjrueng2/version.php
+++ modules/customerdjrueng2/version.php
@@ -3,14 +3,14 @@ if (!defined('NV_ADMIN') or !defined('NV_MAINFILE'))
     die('Stop!!!');
  
 $module_version = array(
-    'name' => 'cus_djrueng', // Tieu de module
+    'name' => 'customer_djrueng2', // Tieu de module
     'modfuncs' => 'main', // Cac function co block
     'change_alias' => '',
     'submenu' => '',
     'is_sysmod' => 0, // 1:0 => Co phai la module he thong hay khong
     'virtual' => 0, // 1:0 => Co cho phep ao hao module hay khong
     'version' => '4.0.00', // Phien ban cua modle
-    'date' => 'Wed, 16 Mar 2018 18:50:00 GMT', // Ngay phat hanh phien ban
+    'date' => 'Wed, 21 Mar 2018 22:33:00 GMT', // Ngay phat hanh phien ban
     'author' => 'hoctap.vidoweb.com.vn (djruengnay@gmail.com)', // Tac gia
     'note' => '', // Ghi chu
     'uploads_dir' => array(
